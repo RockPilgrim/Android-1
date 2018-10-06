6ad6421c32e09e2690aeed1cee62fbc057a00e4d
TimaPilgrim <timatok@gmail.com>
2018-10-06 23:27:55 +0500

    Realized workout window

---
+++ Lesson_2SeptemberWorkout/app/src/main/java/my/lessons/lesson_2septemberworkout/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package my.lessons.lesson_2septemberworkout.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import my.lessons.lesson_2septemberworkout.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
